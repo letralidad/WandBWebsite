8c2d61094abd588adc94f2533b954b931e4bbbd7
Keano Kim <75988307+mrslayer08@users.noreply.github.com>
2021-05-15 12:59:27 +0800

    pinarespansib

--- test/signup.php
+++ test/signup.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>W&B</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="nav_style.css">
     <link rel="stylesheet" type="text/css" href="signup.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/Logo.ico"/> 

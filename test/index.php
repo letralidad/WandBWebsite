59c8fca061339607fda9988bc18f193b1a0f55c9
letralidad <61323894+letralidad@users.noreply.github.com>
2021-05-15 11:57:01 +0800

    Revert "Revert "index.php and index_style.php revision""
    This reverts commit 2fd2f6f92a6c40e44d8aea52e18de6bf8b1d9bde.

--- test/index.php
+++ test/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>W&B</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="index_style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/Logo.ico"/> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -18,7 +18,7 @@
         </div>
 
         <ul class="nav-links">
-            <li><a href="#">HOME</a></li>
+            <li><a href="index.php">HOME</a></li>
             <li><a href="#">MENU</a></li>
             <li><a href="#">ABOUT</a></li>
             <li><a href="#">MY ACCOUNT</a></li>
@@ -30,6 +30,7 @@
         </div>
     </nav>
 
+    <div class="col">
         <h1>WINGS & BEERS</h1>
         <h2>CHICKEN JOINT</h2>
         <button type="button" class="button">login now</button>

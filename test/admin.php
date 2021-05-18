9d6cb850e8faee1e2a4b69d8cd5d5d0571ee0bbb
stehfyyy <84142415+stehfyyy@users.noreply.github.com>
2021-05-18 17:06:17 +0700

    all
    all files are modified due to foldering styles

--- test/admin.php
+++ test/admin.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <title>W&B</title>
-    <link rel="stylesheet" type="text/css" href="style/nav_style.css">
-    <link rel="stylesheet" type="text/css" href="style/login.css">
+    <title>W&B - Admin</title>
+    <link rel="stylesheet" type="text/css" href="style/admin_style.css">
+    <link rel="stylesheet" type="text/css" href="style/nav_s.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/Logo.ico"/> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
 <body>
-<nav>
+    <nav>
         <div class="logo">
             <img src="img/Logo.png" class="logo" alt="logo">
         </div>
@@ -22,7 +22,7 @@
             <li><a href="index.php">HOME</a></li>
             <li><a href="menu.php">MENU</a></li>
             <li><a href="about.php">ABOUT</a></li>
-            <li><a href="login.php"><button type="button" class="button_home">MY ACCOUNT</button></a></li>
+            <li><a href="#"><button type="button" class="button_account">MY ACCOUNT</button></a></li>
         </ul>
         <div class="burger">
             <div class="line1"></div>
@@ -31,26 +31,15 @@
         </div>
     </nav>
 
-    <!-- Login form -->
-    <div class="contentBox">
-        <div class="formBox">
-            <h2>Welcome</h2>
-            <p>Log in now to find the best deals in town</p>
-            <div class="inputBox">
-                <input type="text" name="" placeholder="username">
-            </div>
-            <div class="inputBox">
-                <input type="password" name="" placeholder="password">
-            </div>
-            <div class="forgot">
-                <a href="#"><label>forgot password?</label></a>
-            </div>
-            <div class="ssBox">
-                <input type="submit" value="Sign In" name="">
-                <a href="signup.php"><button type="button">Sign Up</button></a>
-            </div>
-        </div>
+    <!--Side bar-->
+    <div class="sidebar">
+        <a class="admin" href="admin.php">Hi Admin!</a>
+        <a href="incoming_order.php">Incoming Order</a>
+        <a href="current_order.php">Current Order</a>
+        <a href="history_order.php">History of Orders</a>
+        <a href="index.php">Logout</a>
     </div>
+
     <script src="app.js"></script>
 </body>
 </html>

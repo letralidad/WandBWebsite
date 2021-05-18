e25b4333a15ca87b76847c11210fa78142e5a872
stehfyyy <84142415+stehfyyy@users.noreply.github.com>
2021-05-18 22:14:29 +0700

    all
    New admin and order tracker with their css. Please check immediately if there are any updates. Thank you

--- test/incoming_order.php
+++ test/incoming_order.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <title>W&B - Incoming Order</title>
-    <link rel="stylesheet" type="text/css" href="style/incoming_order_style.css">
+    <link rel="stylesheet" type="text/css" href="style/incoming_style.css">
     <link rel="stylesheet" type="text/css" href="style/nav_s.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/Logo.ico"/> 
@@ -33,8 +33,8 @@
 
     <!--Side bar-->
     <div class="sidebar">
-        <a href="admin.php">Hi Admin!</a>
-        <a class = "incoming" href="incoming_order.php">Incoming Orders</a>
+        <h1>Hi Admin!</h1>
+        <a class = "incoming" href="incoming_order.php">Incoming Order</a>
         <a href="current_order.php">Current Order</a>
         <a href="history_order.php">History of Orders</a>
         <a href="index.php">Logout</a>

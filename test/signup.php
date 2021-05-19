84a9cb6354095f5347dc0bdd8a38fedc283b69c1
letralidad <61323894+letralidad@users.noreply.github.com>
2021-05-19 16:45:28 +0800

    login.php and signup.php
    Just put the code of Keano from his commit into his branch.

--- test/signup.php
+++ test/signup.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <title>W&B</title>
-    <link rel="stylesheet" type="text/css" href="style/nav_style.css">
-    <link rel="stylesheet" type="text/css" href="style/signup.css">
+    <link rel="stylesheet" type="text/css" href="nav_style.css">
+    <link rel="stylesheet" type="text/css" href="signup.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/Logo.ico"/> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -31,34 +31,61 @@
         </div>
     </nav>
     <!-- sign up form -->
-    <div class="contentBox">
-        <div class="formBox">
+    <?php require "include/dbcon.php";?>
+    <div  class="contentBox">
+        <?php
+            if(isset($_GET['error'])){
+                $error = $_GET['error'];
+                if($error == 'firstname'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'lastname'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'mobile number'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'address'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'email'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'password'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+                elseif($error == 'click'){
+                    echo "<script>alert('Enter all the required fields');</script>";
+                }
+            }
+        ?>
+        <form action="include/proccess.php" method="post" class="formBox">
             <div class="textbx">
                 <h2>Sign Up</h2>
                 <p>Let's get started!</p>
             </div>
             <div class="inputBox">
-                <input type="text" name="" placeholder="firstname">
+                <input type="text" name="firstname" placeholder="firstname" require>
             </div>
             <div class="inputBox">
-                <input type="text" name="" placeholder="lastname">
+                <input type="text" name="lastname" placeholder="lastname" require>
             </div>
             <div class="inputBox">
-                <input type="tel" name="" placeholder="mobilenumber">
+                <input type="tel" name="mobilenumber" placeholder="mobilenumber" require>
             </div>
             <div class="inputBox">
-                <input type="text" name="" placeholder="address">
+                <input type="text" name="address" placeholder="address" require>
             </div>
             <div class="inputBox">
-                <input type="password" name="" placeholder="password">
+                <input type="email" name="email" placeholder="Email" require>
             </div>
             <div class="inputBox">
-                <input type="password" name="" placeholder="confirm password">
+                <input type="password" name="password" placeholder="password" require>
             </div>
             <div class="ssBox">
-                <a href="login.php"><button type="button">Sign Up</button></a>
+            <a href="login.php"><button type="submit" name="signup">Sign Up</button></a>
             </div>
-        </div>
+        </form>
     </div>
     <script src="app.js"></script>
 </body>

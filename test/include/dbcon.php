7d648edbcbdd8ada9726bcdbe5be7f962e6af05a
letralidad <61323894+letralidad@users.noreply.github.com>
2021-05-19 17:00:18 +0800

    signup.php and include folder
    just change the path of css for signup.php and add include folder with the folder of queries

---
+++ test/include/dbcon.php
@@ -0,0 +1,8 @@
+<?php
+
+$serverName = "localhost:3307";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "wandbuseracc";
+
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);

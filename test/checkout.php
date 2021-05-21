9a0348338f7049077decac0ac9d0610d69f95518
stehfyyy <84142415+stehfyyy@users.noreply.github.com>
2021-05-21 15:12:34 +0700

    checkout.php checkout_style.css
    checkout for the user's order

---
+++ test/checkout.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>W&B - Checkout</title>
+    <link rel="stylesheet" type="text/css" href="style/checkout_style.css">
+    <link rel="stylesheet" type="text/css" href="style/nav_s.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/Logo.ico"/> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="img/Logo.png" class="logo" alt="logo">
+        </div>
+
+        <ul class="nav-links">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="menu.php">MENU</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="#"><button type="button" class="button_account">MY ACCOUNT</button></a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+
+    <!--Side bar-->
+    <div class="sidebar">
+        <h1>Hi User!</h1>
+        <a href="acc_menu.php">Menu</a>
+        <a href="track_your_order.php">Track your order</a>
+        <a href="history_user.php">Order History</a>
+        <a href="acc_management.php">Account Management</a>
+        <a href="index.php">Logout</a>
+    </div>
+
+    <!--content-->
+    <div class="content">
+        <h2>Checkout your order</h2><hr>
+        <div class="content1">
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+        </div>
+        <button class="btn">checkout</button> 
+    </div>
+
+    <script src="app.js"></script>
+</body>
+</html>

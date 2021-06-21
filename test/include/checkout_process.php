e091816667a3235759501a2eccb1449890da534b
Keano Kim <75988307+mrslayer08@users.noreply.github.com>
2021-06-21 23:17:25 +0800

    Update checkout_process.php
    reset all orders from checkout

--- test/include/checkout_process.php
+++ test/include/checkout_process.php
@@ -14,9 +14,11 @@
             //order status always waiting
             $orderstatus = "waiting";
 
+
+            $tablename = "ac" . strval($id);
             $activeSql = $dbOrdersconn->query("INSERT INTO active (orderdate,totalprice,userordered,orderstatus) VALUES('$today','$totalprice','$id','$orderstatus')") or die($mysqli->error);
             $allOrderssql = $dbOrdersconn->query("INSERT INTO allorders (orderdate,totalprice,userordered,orderstatus) VALUES('$today','$totalprice','$id','$orderstatus')") or die($mysqli->error);
-
+            $delete = $dbAddtoCartconn->query("DELETE FROM ac33 WHERE price=199");
             header("Location: ../track_your_order.php");
             exit();
         }

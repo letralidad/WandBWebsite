08435bff6435c6b70d24f3f44ac7e45e163c931b
Keano Kim <75988307+mrslayer08@users.noreply.github.com>
2021-06-21 22:45:29 +0800

    revisions
    checkout , acc menu revised

--- test/include/acc_menu_process.php
+++ test/include/acc_menu_process.php
@@ -13,26 +13,29 @@
 
             $qty_data= array($ec1_qty, $ec2_qty, $ec3_qty, $ec4_qty,$ec5_qty, $ec6_qty);
             
-            $result = $dbAddtoCartconn->query("SELECT * FROM items") or die($mysqli->error);
+            $result = $dbAddtoCartconn->query("SELECT itemcode,itemname,itemprice FROM items") or die($mysqli->error);
             $id = $_SESSION['id'];
             
         
             $getId = mysqli_insert_id($conn);
             $tablename = "ac" . strval($id);
-
+            $totalprice = 0;
                 for($i = 0; $i < 6;){
-                while($row = $result->fetch_assoc()){
-                    $itemcode = $row['itemcode'];
-                    $itemname = $row['itemname'];
-                    $itemprice = $row['itemprice'];
-                    $amount = $itemprice * $qty_data[$i];
-    
-                    $insertsql = $dbAddtoCartconn->query("INSERT INTO $tablename(itemcode,itemname,price,qty,amount) VALUES('$itemcode','$itemname','$itemprice','$qty_data[$i]','$amount')") or die($mysqli->error);
-                    $i++;
+                    while($row = $result->fetch_assoc()){
+                            $itemcode = $row['itemcode'];
+                            $itemname = $row['itemname'];
+                            $itemprice = $row['itemprice'];                   
+                            if($qty_data[$i] > 0){
+                                $amount = $itemprice * $qty_data[$i];
+                                $totalprice += $amount;
+                                $insertsql = $dbAddtoCartconn->query("INSERT INTO $tablename(itemcode,itemname,price,qty,amount) VALUES('$itemcode','$itemname','$itemprice','$qty_data[$i]','$amount')") or die($mysqli->error);          
+                            }                 
+                            $i++;
+                    }         
                 }
-            }
+                $_SESSION['totalprice'] = $totalprice; 
             //pakilagyan ordered successfuly
-            header("Location: ../index.php");
+            header("Location: ../checkout.php");
             exit();
         }
     }
